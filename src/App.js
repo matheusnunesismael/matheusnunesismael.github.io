faed9c4092195414fae086fb5dd79e0186d1d95c
matheusnunesismael <nunes.matheus.ismael@gmail.com>
2023-02-21 18:48:12 -0300

    Arquivos da build

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         sx={{
           width: "100%",
           height: "100%",
-          minHeight: "100vh",
+          minHeight: "90vh",
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "row",
@@ -87,6 +87,31 @@ function App() {
             ))}
           </List>
         </Stack>
+      </Stack>
+      <Stack
+        sx={{
+          display: "flex",
+          width: "100%",
+          height: "10vh",
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Link
+          href="https://www.buymeacoffee.com/numnes"
+          target={"_blank"}
+          rel="noreferrer"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=numnes&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
+            alt="Buy Me a Coffe"
+          />
+        </Link>
       </Stack>
     </Stack>
   );
